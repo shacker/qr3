e03775c2ed12be7efb9d851e2191f42058d45b54
Vinay Valsaraj <vvalsaraj@doctorondemand.com>
2018-07-06 15:00:04 -0700

    Fixed bug in dump where 0 was ending the loop and load where loaded contents were not being packed before put in redis.

--- qr.py
+++ qr.py
@@ -243,17 +243,17 @@ class PriorityQueue(BaseQueue):
     
     def dump(self, fobj):
         """Destructively dump the contents of the queue into fp"""
-        next = self.pop()
-        while next:
-            self.serializer.dump(next[0], fobj)
-            next = self.pop()
-    
+        next = self.pop(True)
+        while next[0] is not None:
+            self.serializer.dump(next, fobj)
+            next = self.pop(True)
+
     def load(self, fobj):
         """Load the contents of the provided fobj into the queue"""
         try:
             while True:
                 value, score = self.serializer.load(fobj)
-                self.redis.zadd(self.key, value, score)
+                self.push(value, score)
         except Exception as e:
             return
     
